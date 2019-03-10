e7efdc15b3dfefa16c26aabe6a8ee708fc5c9020
ukh <able.kiho.um@gmail.com>
2019-03-10 20:56:31 +0900

    first commit..

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//var React = require('react'); 
+
+class App extends React.Component {
+    render() {
+        return (
+            <h1>Hello</h1>
+        )
+    }
+}
+
+export default App;
+//module.export = App;
